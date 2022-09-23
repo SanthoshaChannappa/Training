032b84d39f17f37cd3c9fffa6af6057413b3eecb
Santhosha <santhosha.mandya@hotmail.com>
2022-09-23 20:02:43 +0530

    Reflections sample program

---
+++ SampleCode/Reflections/ReflectionsAndGenerics/ReflectionsAndGenerics/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ReflectionsAndGenerics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var p = Person.GetNewPerson();
+
+            var t = p.GetType();
+
+            foreach (var prop in t.GetProperties())
+            {
+                Console.WriteLine("{0} -> {1}", prop.Name, prop.GetValue(p));
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public float Salary { get; set; }
+
+        public static Person GetNewPerson()
+        {
+            Random r = new Random();
+            var id = r.Next();
+            return new Person()
+            {
+                Id = id,
+                Name = $"User {id}",
+                CreatedOn = DateTime.Now,
+                Salary = r.NextSingle() * 10000,
+            };
+        }
+    }
+}
